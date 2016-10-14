c71583b6c7083c3eb31e7adb0012f427ad3c15ef
Dillon <dragdi15@gmail.com>
2016-10-14 14:27:52 -0400

    Add files via upload

---
+++ lab6task2.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+vector<int> merge_vector(vector<int> v0, vector<int> v1);
+
+int main()
+{
+    vector<int> Merge;
+    vector<int> vec0(2);
+    vector<int> vec1(3);
+    vector<int> vec2(7);
+    int Time = time(0);
+    cout << "The elements of vector 0:" << endl;
+        for (int i=0;i<2;i++)
+    {
+        srand(Time + i);
+        vec0[i] = rand()%13 + 2;
+        cout << vec0[i] << endl;
+    }
+    cout << "The elements of vector 1:" << endl;
+        for (int i=0;i<3;i++)
+    {
+        srand(Time + i + 2);
+        vec1[i] = (rand()%13+ 2);
+        cout << vec1[i] << endl;
+    }
+    cout << "The elements of vector 2:" << endl;
+        for (int i=0;i<7;i++)
+    {
+        srand(Time + i + 5);
+        vec2[i] = (rand()%13 + 2);
+        cout << vec2[i] << endl;
+    }
+    cout << "The elements of the vector after merging vector 0 and vector 1:" << endl;
+    int length1=vec0.size();
+    int length2=vec1.size();
+    Merge = merge_vector(vec0,vec1);
+    for(int i=0;i<length1+length2;i++)
+    {
+        cout << Merge[i] << endl;
+    }
+        cout << "The elements of the vector after merging vector 2 and vector 1:" << endl;
+    length1=vec2.size();
+    length2=vec1.size();
+    Merge = merge_vector(vec2,vec1);
+    for(int i=0;i<length1+length2;i++)
+    {
+        cout << Merge[i] << endl;
+    }
+
+    return 0;
+}
---
+++ lab6task2_merge_elements.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+vector<int> merge_vector(vector<int> v0, vector<int> v1)
+{
+    vector<int> Merged_Vector(10);
+    int v0_Size = v0.size();
+    int v1_Size = v1.size();
+    //Determine case to apply algorithm of merging
+    //Case 1 V0 is bigger
+    if(v1_Size>v0_Size)
+    {
+        for(int i=0;i<v0_Size;i++)
+        {
+            Merged_Vector[2*i] = v0[i];
+            Merged_Vector[2*i+1] = v1[i];
+        }
+        for(int i=0;i<v1_Size;i++)
+        {
+            Merged_Vector[2*v0_Size+i] = v1[v0_Size+i];
+        }
+    }
+    //Case 2 V1 is bigger
+    else if(v0_Size>v1_Size)
+    {
+        for(int i=0;i<v1_Size;i++)
+        {
+            Merged_Vector[2*i] = v0[i];
+            Merged_Vector[2*i+1] = v1[i];
+        }
+        for(int i=0;i<v0_Size;i++)
+        {
+            Merged_Vector[2*v1_Size+i] = v0[v1_Size+i];
+        }
+    }
+    //Case 3 Same Size
+    else
+    {
+        for(int i=0;i<v0_Size;i++)
+        {
+            Merged_Vector[2*i] = v0[i];
+            Merged_Vector[2*i+1] = v1[i];
+        }
+    }
+    return (Merged_Vector);
+}
+
